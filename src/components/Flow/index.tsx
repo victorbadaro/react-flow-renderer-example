09d27fad2be071d2026b642ea49483a1b60a7f0a
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-09-30 01:22:19 -0300

    feat: add a form to add new nodes into the flow

--- src/components/Flow/index.tsx
+++ src/components/Flow/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import ReactFlowRenderer, { Background, Node } from 'react-flow-renderer';
 
 const initialNodes: Node[] = [
@@ -15,17 +16,48 @@ const initialNodes: Node[] = [
 ];
 
 export function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [newNodeLabel, setNewNodeLabel] = useState('');
+
+  function handleFormSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const newNode: Node = {
+      id: String(nodes.length + 1),
+      data: {
+        label: newNodeLabel
+      },
+      position: {
+        x: Math.random() * window.innerWidth,
+        y: Math.random() * window.innerHeight
+      }
+    };
+
+    setNodes((previousNodes) => [ ...previousNodes, newNode ]);
+    setNewNodeLabel('');
+  }
+
   return (
     <>
       <ReactFlowRenderer
-        defaultNodes={initialNodes}
+        defaultNodes={nodes}
         style={{
-          height: '500px',
-          backgroundColor: '#222'
+          height: '500px'
         }}
       >
         <Background />
       </ReactFlowRenderer>
+
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="New node label here..."
+          onChange={(event) => setNewNodeLabel(event.target.value)}
+          value={newNodeLabel}
+        />
+
+        <button type="submit">Add new node</button>
+      </form>
     </>
   );
 }
