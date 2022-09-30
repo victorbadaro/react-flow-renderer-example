4efbb32027658ceb9483bae0453dbac84aa9bac6
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-09-30 02:02:56 -0300

    feat: add eslint dependencies and its configs

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Flow } from './components/Flow';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <h1>Hello, React Flow Renderer Example!</h1>
-      <Flow />
-    </>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<h1>Hello, React Flow Renderer Example!</h1>
+			<Flow />
+		</>
+	);
 }
--- src/components/Flow/index.tsx
+++ src/components/Flow/index.tsx
@@ -2,87 +2,87 @@ import { FormEvent, useCallback, useState } from 'react';
 import ReactFlow, { addEdge, Background, Connection, Edge, Node, useEdgesState, useNodesState } from 'react-flow-renderer';
 
 const initialNodes: Node[] = [
-  {
-    id: '1',
-    data: {
-      label: 'First Node'
-    },
-    position: {
-      x: 0,
-      y: 0
-    },
-  },
-  {
-    id: '2',
-    data: {
-      label: 'Second Node'
-    },
-    position: {
-      x: 400,
-      y: 400
-    },
-  }
+	{
+		id: '1',
+		data: {
+			label: 'First Node'
+		},
+		position: {
+			x: 0,
+			y: 0
+		},
+	},
+	{
+		id: '2',
+		data: {
+			label: 'Second Node'
+		},
+		position: {
+			x: 400,
+			y: 400
+		},
+	}
 ];
 const initialEdges: Edge[] = [
-  {
-    id: crypto.randomUUID(),
-    source: '1',
-    target: '2'
-  }
+	{
+		id: crypto.randomUUID(),
+		source: '1',
+		target: '2'
+	}
 ];
 
 export function Flow() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const [newNodeLabel, setNewNodeLabel] = useState('');
+	const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+	const [newNodeLabel, setNewNodeLabel] = useState('');
 
-  const onConnect = useCallback((params: Edge | Connection) => {
-    setEdges((previousEdges) => addEdge(params, previousEdges));
-  }, [setEdges]);
+	const onConnect = useCallback((params: Edge | Connection) => {
+		setEdges((previousEdges) => addEdge(params, previousEdges));
+	}, [setEdges]);
 
-  function handleFormSubmit(event: FormEvent) {
-    event.preventDefault();
+	function handleFormSubmit(event: FormEvent) {
+		event.preventDefault();
 
-    const newNode: Node = {
-      id: crypto.randomUUID(),
-      data: {
-        label: newNodeLabel
-      },
-      position: {
-        x: Math.random() * window.innerWidth,
-        y: Math.random() * window.innerHeight
-      }
-    };
+		const newNode: Node = {
+			id: crypto.randomUUID(),
+			data: {
+				label: newNodeLabel
+			},
+			position: {
+				x: Math.random() * window.innerWidth,
+				y: Math.random() * window.innerHeight
+			}
+		};
 
-    setNodes((previousNodes) => [ ...previousNodes, newNode ]);
-    setNewNodeLabel('');
-  }
+		setNodes((previousNodes) => [ ...previousNodes, newNode ]);
+		setNewNodeLabel('');
+	}
 
-  return (
-    <>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        style={{
-          height: '500px'
-        }}
-      >
-        <Background />
-      </ReactFlow>
+	return (
+		<>
+			<ReactFlow
+				nodes={nodes}
+				edges={edges}
+				onNodesChange={onNodesChange}
+				onEdgesChange={onEdgesChange}
+				onConnect={onConnect}
+				style={{
+					height: '500px'
+				}}
+			>
+				<Background />
+			</ReactFlow>
 
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          placeholder="New node label here..."
-          onChange={(event) => setNewNodeLabel(event.target.value)}
-          value={newNodeLabel}
-        />
+			<form onSubmit={handleFormSubmit}>
+				<input
+					type="text"
+					placeholder="New node label here..."
+					onChange={(event) => setNewNodeLabel(event.target.value)}
+					value={newNodeLabel}
+				/>
 
-        <button type="submit">Add new node</button>
-      </form>
-    </>
-  );
+				<button type="submit">Add new node</button>
+			</form>
+		</>
+	);
 }
