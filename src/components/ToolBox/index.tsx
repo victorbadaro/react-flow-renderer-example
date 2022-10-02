8af9d4fff3d185a2474e11647f3067f58ece7e9a
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-10-02 00:04:05 -0300

    feat: add function to the ToolBox buttons to create new nodes

--- src/components/ToolBox/index.tsx
+++ src/components/ToolBox/index.tsx
@@ -1,13 +1,37 @@
+import { useReactFlowStore } from '@/stores/useReactFlowStore';
+import { Node } from 'react-flow-renderer';
 import { FiGithub, FiGitlab } from 'react-icons/fi';
 import { ToolBoxContainer } from './styles';
 
 export function ToolBox() {
+	const addNode = useReactFlowStore((state) => state.addNode);
+
+	function handleToolButtonClick(nodeType: string) {
+		const newNode: Node = {
+			id: crypto.randomUUID(),
+			type: nodeType,
+			data: {},
+			position: {
+				x: 0,
+				y: 0
+			}
+		};
+
+		addNode(newNode);
+	}
+
 	return (
 		<ToolBoxContainer>
-			<button type="button">
+			<button
+				type="button"
+				onClick={() => handleToolButtonClick('github')}
+			>
 				<FiGithub />
 			</button>
-			<button type="button">
+			<button
+				type="button"
+				onClick={() => handleToolButtonClick('gitlab')}
+			>
 				<FiGitlab />
 			</button>
 		</ToolBoxContainer>
