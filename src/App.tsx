e22ceaa9104bc4024d27e0d88296ada93a46c075
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-10-01 22:35:44 -0300

    refactor: remove files that will be replaced with different content

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { ReactFlowProvider } from 'react-flow-renderer';
-import { AddDefaultNodeButton } from './components/AddDefaultNodeButton';
-import { AddTriggerNodeButton } from './components/AddTriggerNodeButton';
-import { Button } from './components/Button';
-import { Flow } from './components/Flow';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
 	return (
 		<>
 			<GlobalStyle />
-			<h1>Hello, React Flow Renderer Example!</h1>
-			<ReactFlowProvider>
-				<Flow />
-				<Button />
-				<AddDefaultNodeButton />
-				<AddTriggerNodeButton />
-			</ReactFlowProvider>
 		</>
 	);
 }
