8dc8d8f91554e709bd062aad71f3c373a287dabc
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-10-02 00:27:30 -0300

    feat: add github corner

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,10 +1,17 @@
 import { Header } from '@/components/Header';
+import GitHubCorner from 'react-github-corner';
 import { Outlet } from 'react-router-dom';
 import { AppLayoutContainer } from './styles';
 
 export function AppLayout() {
 	return (
 		<AppLayoutContainer>
+			<GitHubCorner
+				href="https://github.com/victorbadaro/react-flow-renderer-example"
+				target="_blank"
+				bannerColor="#111"
+				octoColor="#222"
+			/>
 			<Header />
 			<Outlet />
 		</AppLayoutContainer>
