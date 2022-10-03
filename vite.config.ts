dcfa683378ae9017227986d8ba8a288479606d8f
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-10-03 11:53:47 -0300

    ci: fix deploy configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { viteSingleFile } from 'vite-plugin-singlefile';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteSingleFile(), tsconfigPaths()],
-	base: '/'
+	plugins: [react(), tsconfigPaths()],
+	base: '/react-flow-renderer-example/'
 });
