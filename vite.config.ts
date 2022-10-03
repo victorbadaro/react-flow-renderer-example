cc21bec3922717714ab5ca27452e36d57dc14ead
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-10-03 11:11:34 -0300

    feat: add vite plugin single file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	plugins: [react(), viteSingleFile(), tsconfigPaths()],
 	base: '/'
 });
